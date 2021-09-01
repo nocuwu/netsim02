f3ae5fdc184a05a66891a5d9c2c80933c5b7f636
nocuwu <40336389+nocuwu@users.noreply.github.com>
2021-09-01 23:39:03 +0900

    read txt

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
+Data = open('data.txt', "r")
+next(Data, None)
+Graphtype = nx.Graph()
 
-edges = [
-         ("0: src",1),
-         ("0: src",2),
-         (1,2),
-         (1,"3: dst"),
-         (2,"3: dst")
-        ]
-
-G.add_edges_from(edges)
+G = nx.read_weighted_edgelist(Data)
 
 nx.draw(G, with_labels=True, font_size=10, node_size=2000, font_color="w")
 plt.savefig("nx_tmp.png")
