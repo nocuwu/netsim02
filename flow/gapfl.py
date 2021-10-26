42dfc177e60ea181bcc86306b9b85e9eb8eefa39
nocuwu <40336389+nocuwu@users.noreply.github.com>
2021-10-26 17:26:23 +0900

    m: gap, m2: flow

--- flow/gapfl.py
+++ flow/gapfl.py
@@ -56,42 +56,52 @@ def gap(cst, req, cap):
     for j in range(nj):
         m += lpSum(v[i][j] for i in range(na)) == 1
 
-    ##フロー保存則
+    if m.solve() != 1:
+        return None
+
     s = 1  # 始点
-    t = 10  # 終点
+    t = 4  # 終点
     df = pd.read_csv('flow.csv')
 
     # CSVの各行Lに対して、変数xi_jを生成し、
     # 新しい列'Var'として追加している。
-    df['Var'] = [pulp.LpVariable(f'x{df.i[L]}_{df.j[L]}', 0, df.c[L])
-        for L in df.index]
-    z = pulp.LpVariable('z')
+    df['Var'] = [pulp.LpVariable(f'x{df.i[L]}_{df.j[L]}', 0, df.c[L]) for L in df.index]
+    ##z = pulp.LpVariable('z')
 
-    m += z, '目的関数'  # zの実体は以下の制約条件内で定義する
+    m2 = pulp.LpProblem('最大流問題', sense=pulp.LpMinimize)
+    m2 += pulp.lpDot(df.c, df.Var), '目的関数'  # zの実体は以下の制約条件内で定義する
+
+    # set(df.i)はi列の一覧、set(df.j)はj列の一覧。|でつないだら、その和集合
     # set(df.i)はi列の一覧、set(df.j)はj列の一覧。|でつないだら、その和集合
     for n in set(df.i)|set(df.j):
         if n == s:
-            m += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == z, f'始点{n}'
+            m2 += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == 1, f'始点{n}'
         elif n == t:
-            m += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == -z, f'終点{n}'
+            m2 += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == -1, f'終点{n}'
         else:
-            m += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == 0, f'点{n}'
+            m2 += pulp.lpSum(df.Var[df.i==n]) - pulp.lpSum(df.Var[df.j==n]) == 0, f'点{n}'
 
     # print(na)
     # print(nj)
     # print(len(cst[0]))
 
-    ## 解けなかったらnone
-    if m.solve() != 1:
-        return None
     '''
     for i in range(na):
         for j in range(nj):
             print(value(v[i][j]))
     '''
 
-    ## それ以外（解けたら）return
-    print(pulp.value(m.objective))
+
+    # 制約条件をすべて登録したので解を求める
+    result = m2.solve(pulp.PULP_CBC_CMD(msg = False))
+    # LpStatusが`optimal`なら最適解が得られた事になる
+    print(pulp.LpStatus[result])
+    # 目的関数の値
+    print(pulp.value(m2.objective))
+    # 'Var'変数の結果の値をまとめて'Val'列にコピーしている
+    df['Val'] = df.Var.apply(pulp.value)
+    # 結果表示
+    print(df)
     return [
         int(value(lpDot(range(na), [v[i][j] for i in range(na)]))) for j in range(nj)
     ]
