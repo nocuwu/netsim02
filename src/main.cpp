53d3fbf2b759eb4367dcdc6456b2d16fdba25312
nocuwu <40336389+nocuwu@users.noreply.github.com>
2021-10-28 22:52:56 +0900

    up

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,51 @@
 
 using namespace std;
 
-#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+#define rep(i, n) for (int(i) = 0; (i) < (n); (i)++)
 
 typedef long long ll;
 typedef vector<int> vi;
-// サーバ1つ当たりのCPUコア（）
-#define cpu_core 10
-#define memory 100
-// スループット（サーバ間）
-#define tp 100
 
-struct vnf(){
-    int req_cpu = 2;
-    int req_memory = 10;
+// サーバ1つ当たりのCPUコア数
+#define cpu_core 12
+// サーバ1つ当たりのメモリ(RAM)[GB]
+#define ram 24
+// スループット（source-サーバ間/サーバ-terminal間）[bps]
+#define tp_inout_server 1000000000
+// スループット（サーバ間）[bps]
+#define tp_server_server 100000000
+
+int a = 1;
+int b = 1;
+
+struct sc
+{
+  int req_bw;
+  int req_cpu_sc;
+  int req_ram_sc;
+};
+
+struct vnf
+{
+  int req_cpu;
+  int req_ram;
+};
+
+int main()
+{
+  //データサイズが変化するsc
+  sc variable;
+  variable.req_bw = 100000000;
+  //データサイズが変化しないsc
+  sc no_compression;
+  no_compression.req_bw = 100000000;　
+
+  return 0;
 }
 
-int main(){
-	
-    
-    return 0;
-}
-
-int gcd(int a, int b) {
-  if (b == 0) 
-    return a;  
+int gcd(int a, int b)
+{
+  if (b == 0)
+    return a;
   return gcd(b, a % b);
 }
