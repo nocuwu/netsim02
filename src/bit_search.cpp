7dc98affc2e43487cba6925b924f9a3c711dd3b8
nocuwu <40336389+nocuwu@users.noreply.github.com>
2021-11-08 18:16:05 +0900

    6bit

--- src/bit_search.cpp
+++ src/bit_search.cpp
@@ -1,3 +1,4 @@
+#include <wchar.h>
 #include <bits/stdc++.h>
 #include <iostream>
 #include <algorithm>
@@ -9,9 +10,13 @@
 
 using namespace std;
 
+int bin2dec(int bin_1, int bin_0){
+    return (bin_1 * bin_1) + bin_0;
+}
+
 int main()
 {
-    int n = 5;
+    int n = 6;
     int W = 10;
     vector<int> a(n);
 
@@ -21,20 +26,36 @@ int main()
 
     int ans = 0;
 
+    //組み合わせ数ループ
     for (int bit = 0; bit < (1 << n); ++bit)
     {
         int S = 0;
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i = i + 2)
         {
+            int bin_0, bin_1;
             //整数bitを2進法表記したときの、下からi桁目が1か判定
+            //cout << (bit & (1 << i));
             if (bit & (1 << i))
             {
-                cout << 1;
+                if (bit & (1 << i+1)){
+                    cout << 3;
+                }
+                else{
+                    cout << 1;
+                }
+                //cout << 1;
                 //S += a[i];
             }
             else{
-                cout << 0;
+                if (bit & (1 << i+1)){
+                    cout << 2;
+                }
+                else{
+                    cout << 0;
+                }
+                //cout << 0;
             }
+            
         }
         if (S == W)
         {
@@ -43,6 +64,4 @@ int main()
         }
         cout << ":"<< bit << endl;
     }
-
-    cout  << ans << endl;
 }
